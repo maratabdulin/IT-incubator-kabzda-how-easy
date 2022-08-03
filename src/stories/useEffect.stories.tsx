da34ffc6cb88be2c4158b7d9d53f553f7c0fdafe
Marat <marato13@ya.ru>
2022-08-03 11:46:16 +0300

    add 21 lesson about how to use useEffect

---
+++ src/stories/useEffect.stories.tsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+
+export default {
+    title: 'UseEffect'
+}
+
+
+export const Example2 = () => {
+
+    const [counter, setCounter] = useState(1);
+
+    useEffect(() => {
+        console.log('useEffect every render')
+        document.title = counter.toString();
+    });
+
+    useEffect(() => {
+        console.log('useEffect only first render (componentDidMount)')
+        document.title = counter.toString();
+    }, []);
+
+    useEffect(() => {
+        console.log('useEffect first render and every counter change')
+        document.title = counter.toString();
+    },[counter]);
+
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        {counter}
+    </>
+}
